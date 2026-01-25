a7d2a3e2f6d4f7e4a8f3b63340d28fb0510c6e2d
Peter Thaleikis <peter.thaleikis@gmail.com>
2026-01-25 17:51:58 +0100

    Run tests on Github

---
+++ .php-cs-fixer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests');
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'declare_strict_types' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
+        'trailing_comma_in_multiline' => true,
+        'single_quote' => true,
+    ])
+    ->setFinder($finder);
